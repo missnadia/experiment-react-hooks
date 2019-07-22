f37d9ce67576538bb3f605676bcd0feb7c77690f
learn <miss.nadia.lee@gmail.com>
2019-07-21 20:31:43 -0400

    Add time to set display:none at end of animation

--- react-hooks/components/Box.js
+++ react-hooks/components/Box.js
@@ -1,22 +1,31 @@
 import React from 'react'
+import { Animated } from 'react-animated-css';
 
 function Box({ word }) {
     const color = colors[Math.floor(Math.random() * 9)];
     const [visible, setVisible] = useState(true);
+
     function hideMe() {
-        setVisible(false);
+        setFading(true)
+        setTimeout(() => setVisible(false), 650)
     }
+
     let style = { borderColor: color, backgroundColor: color };
-    if (!visible) style.display = "none";
+
     return (
-        <div className="box" style={style}>
-            {" "}
-            <div className="center">{word}</div>{" "}
-            <button className="button bottom-corner" onClick={hideMe}>
-                {" "}
-                <i className="center far fa-eye fa-lg" />{" "}
-            </button>{" "}
-        </div>
+        <Animated
+            animationIn="zoomIn"
+            animationOut="zoomOut"
+            isVisible={!fading}
+            style={visible ? null : { display: "none" }}
+        >
+            <div className="box" style={style}>
+                <div className="center">{word}</div>
+                <button className="button bottom-corner" onClick={hideMe}>
+                    <i className="center far fa-eye fa-lg" />
+                </button>
+            </div>
+        </Animated>
     );
 }
 
